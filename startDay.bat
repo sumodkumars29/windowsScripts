2256b49bca8945f626bbe2638e68079075781718
sumodk <sumodkumars29@gmail.com>
2026-01-09 08:51:41 +0530

    added .ahk 'ddmm' trigger for file explorers

--- startDay.bat
+++ startDay.bat
@@ -9,6 +9,7 @@ start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profil
 
 :: Launch Excel with the specified workbook
 start "" excel "%USERPROFILE%\Desktop\Sales Tracker.xlsm"
+start "" "C:\Users\S10DIGITAL\Desktop\autoHotKey\hsDateMonthYear.ahk"
 
 :: start "" — the empty quotes set a blank window title (required when the command path or arguments are quoted).
 ::  --profile-directory="Profile 1" — correct syntax for Chrome profiles.
